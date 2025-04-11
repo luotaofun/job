6fe6fab48ce22daf267be1eff05ee9c0b66a8abf
luotaofun <luotao0213@163.com>
2025-04-11 23:39:13 +0800

    tttt

--- job/mybatis-base/src/main/java/com/luotao/Main.java
+++
@@ -1,7 +0,0 @@
-package com.luotao;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- job/mybatis-base/src/main/java/com/luotao/mapper/EmpMapper.java
+++
@@ -1,9 +0,0 @@
-package com.luotao.mapper;
-
-import com.luotao.domian.Employ;
-
-import java.util.List;
-
-public interface EmpMapper {
-    List<Employ> list();
-}
